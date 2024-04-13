339fb5549505f90cc5a4e7d20127a17661a05cf0
Mohamed Zahran <83869950+MohamedZaahran@users.noreply.github.com>
2024-04-13 16:44:31 +0200

    Add files via upload

---
+++ Creating Local Users/add-new-local-user/add-new-local-user.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+# Check if the script is executed with the root user.
+if [[ "${UID}" -ne 0 ]]
+then
+  echo 'You are not the root user. Execute this script as a root user to proceed.'
+  exit 1
+fi
+
+# Provide usage statement if the user didn't supply an account name on the command line.
+if [[ "${#}" -eq 0 ]]
+then
+  echo "Usage: ${0} USER_NAME [COMMENT]..."
+  exit 1
+fi
+
+# First argument for username, rest for comment.
+USER_NAME=${1}
+shift
+COMMENT=${*}
+
+adduser -c "${COMMENT}" -m ${USER_NAME}
+
+# Checking if the user account was created successfully.
+if [[ "${?}" -ne 0 ]]
+then
+  echo 'An error occurred and cannot add the user.'
+  exit 1
+fi
+
+# Automatically generating a password for the new account.
+PASSWORD=$(date +%s%N | sha256sum | head -c48)
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
+
+# Checking if the password was assigned successfully.
+if [[ "${?}" -ne "0" ]]
+then
+  echo 'An error occurred and cannot assign the password to the user.'
+  exit 1
+fi
+
+# Force the password to be changed upon login.
+passwd -e ${USER_NAME}
+
+# Display the username, password, and hostname where the account was created.
+echo
+echo -e "The username of the account:\n${USER_NAME}"
+echo
+echo -e "The password of the account:\n${PASSWORD}"
+echo
+echo -e "The host name that created that account:\n${HOSTNAME}"
+
+exit 0
