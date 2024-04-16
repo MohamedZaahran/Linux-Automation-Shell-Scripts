2a45cadbc8eec9791fb23ed31db1ded16149854f
Mohamed Zahran <83869950+MohamedZaahran@users.noreply.github.com>
2024-04-16 02:42:26 +0200

    Update extract-open-ports.sh

--- Scripts/extract-open-ports.sh
+++ Scripts/extract-open-ports.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This script shows the open network ports on a system.
+# This script shows the open network ports on a system with sorting them.
 
 # Use -4 as an argument to limit to tcpv4 ports.
-netstat -nutl ${1} | grep ':' | awk '{print $4}' | awk -F ':' '{print $NF}'
+netstat -nutl ${1} | grep ':' | awk '{print $4}' | awk -F ':' '{print $NF}' | sort -un
